9c6b44068a23fabb7962088d7939429c3bb84eff
Ali Irfan <aliirfan4388@gmail.com>
2025-07-28 04:43:55 +0530

    production error fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,11 +12,11 @@ import { app,server } from "./lib/socket.js";
 
 dotenv.config();
 const __dirname = path.resolve();
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
   
 }
